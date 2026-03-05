aa13ef1e8403ed5d68c09d580f221ed5347ae770
Gustavo12386 <gucalderaro@hotmail.com>
2026-03-05 09:43:31 -0300

    alteracao no cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
       'http://localhost:5173',
       'https://agendapro.netlify.app',
     ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    methods: 'GET,PUT,POST,DELETE',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
