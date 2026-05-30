26004c6c5f55035cc536c2a362a6ac3ab2321fff
Gustavo12386 <gucalderaro@hotmail.com>
2026-05-30 23:08:30 +0300

    alteracao no cors

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'https://agendapro.netlify.app',
-    ],
-    methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    origin: true,
     credentials: true,
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
-void bootstrap();
+bootstrap();
