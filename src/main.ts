5eb757ab4099ca3d431b80850eaaba0a248ff22a
Gustavo12386 <gucalderaro@hotmail.com>
2026-06-01 14:44:28 -0300

    alteracao no main.js e no vercel.json

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,40 @@
-import {
-  ExceptionFilter,
-  Catch,
-  ArgumentsHost,
-  HttpException,
-} from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import express from 'express';
+import serverlessExpress from '@codegenie/serverless-express';
 
-@Catch()
-class GlobalExceptionFilter implements ExceptionFilter {
-  catch(exception: any, host: ArgumentsHost) {
-    console.error('ERRO COMPLETO:', exception);
+const expressApp = express();
 
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse();
+let cachedServer: any;
 
-    if (exception instanceof HttpException) {
-      response.status(exception.getStatus()).json({
-        message: exception.message,
-      });
-      return;
-    }
+//função para criar o servidor NestJS e armazená-lo em cache para reutilização em chamadas subsequentes
+async function bootstrap() {
+  if (!cachedServer) {
+    const app = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
 
-    response.status(500).json({
-      message: 'Erro interno',
+    app.enableCors({
+      origin: [
+        'https://agendapro.netlify.app',
+        'http://localhost:5173',
+      ],
+      credentials: true,
+    });
+
+    await app.init();
+
+    cachedServer = serverlessExpress({
+      app: expressApp,
     });
   }
+
+  return cachedServer;
+}
+
+export default async function handler(req: any, res: any) {
+  const server = await bootstrap();
+  return server(req, res);
 }
