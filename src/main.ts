c85adb37ebefa1edbfdd5d69d8ef883f45248064
Gustavo12386 <gucalderaro@hotmail.com>
2026-06-01 18:24:23 -0300

    alteracao no main.js e no vercel.json

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import express from 'express';
-import serverlessExpress from '@codegenie/serverless-express';
 
-const expressApp = express();
-
-let cachedServer: any;
-
-//função para criar o servidor NestJS e armazená-lo em cache para reutilização em chamadas subsequentes
 async function bootstrap() {
-  console.log('NODE_ENV:', process.env.NODE_ENV);
-  console.log('MONGODB_URI EXISTS:', !!process.env.MONGODB_URI);
+  const app = await NestFactory.create(AppModule);
 
-  if (process.env.MONGODB_URI) {
-    console.log(
-      'MONGODB_URI START:',
-      process.env.MONGODB_URI.substring(0, 20),
-    );
-  }
+  app.enableCors({
+    origin: [
+      'https://agendapro.netlify.app',
+      'http://localhost:5173',
+    ],
+    credentials: true,
+  });
 
-  if (!cachedServer) {
-    const app = await NestFactory.create(
-      AppModule,
-      new ExpressAdapter(expressApp),
-    );
-
-    app.enableCors({
-      origin: [
-        'https://agendapro.netlify.app',
-        'http://localhost:5173',
-      ],
-      credentials: true,
-    });
-
-    await app.init();
-
-    cachedServer = serverlessExpress({
-      app: expressApp,
-    });
-  }
-
-  return cachedServer;
+  await app.listen(process.env.PORT || 3000);
 }
 
-export default async function handler(req: any, res: any) {
-  const server = await bootstrap();
-  return server(req, res);
-}
+bootstrap();
