85c5c92e18262af4984f6df80e50ffde0418c0b8
Gustavo12386 <gucalderaro@hotmail.com>
2026-06-01 17:07:44 -0300

    alteracao no main.js e no vercel.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NewprojectModule } from './newproject/newproject.module';
 
-console.log('MONGODB_URI:', process.env.MONGODB_URI);
-
 @Module({  
   imports: [
     MongooseModule.forRoot(process.env.MONGODB_URI as string),
